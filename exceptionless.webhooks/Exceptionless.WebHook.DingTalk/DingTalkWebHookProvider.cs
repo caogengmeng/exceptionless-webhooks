23bf9d13eff23d31f886eaad7c744a5f6807bcc3
李二毛 <lijun@geekbuy.com.cn>
2018-11-16 16:39:30 +0800

    添加环境信息

--- exceptionless.webhooks/Exceptionless.WebHook.DingTalk/DingTalkWebHookProvider.cs
+++ exceptionless.webhooks/Exceptionless.WebHook.DingTalk/DingTalkWebHookProvider.cs
@@ -1,37 +1,45 @@
-﻿using Exceptionless.WebHook.Abstractions;
-using Exceptionless.WebHook.DingTalk.Messages;
-using Exceptionless.WebHook.DingTalk.Services;
+﻿using ExceptionLess.WebHook.Abstractions;
+using ExceptionLess.WebHook.DingTalk.Messages;
+using ExceptionLess.WebHook.DingTalk.Services;
+using Microsoft.AspNetCore.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
-namespace Exceptionless.WebHook.DingTalk
+namespace ExceptionLess.WebHook.DingTalk
 {
     public class DingTalkWebHookProvider : IWebHookProvider
     {
         private readonly FileTemplateService _fileTemplateService;
         private readonly MessageService _messageService;
+        private readonly IHostingEnvironment _environment;
 
-        public DingTalkWebHookProvider(FileTemplateService fileTemplateService, MessageService messageService)
+        public DingTalkWebHookProvider(
+            FileTemplateService fileTemplateService,
+            MessageService messageService,
+            IHostingEnvironment environment)
         {
             _fileTemplateService = fileTemplateService;
             _messageService = messageService;
+            _environment = environment;
         }
 
         #region Implementation of IWebHookProvider
 
         public string Name { get; } = "DingTake";
 
-        public async Task ProcessAsync(ExceptionlessEventModel model, IDictionary<string, string> parameters)
+        public async Task ProcessAsync(ExceptionLessEventModel model, IDictionary<string, string> parameters)
         {
             parameters.TryGetValue("accessToken", out var accessToken);
+
+            model.Environment = _environment.EnvironmentName;
             var content = await _fileTemplateService.GetContent("markdownTemplate.md", model);
 
             await _messageService.SendAsync(
                 new Uri($"https://oapi.dingtalk.com/robot/send?access_token={accessToken}"),
                 new DingTalkRequestMessage
                 {
-                    Data = new MarkdownDingTalkMessage("Exceptionless 有新的事件", content)
+                    Data = new MarkdownDingTalkMessage($"[{_environment.EnvironmentName}]ExceptionLess有新事件", content)
                 });
         }
 
