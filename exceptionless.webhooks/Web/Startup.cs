0d0849f9718eabe9123230e713099d80b471fe76
李二毛 <lijun@geekbuy.com.cn>
2018-11-16 16:40:59 +0800

    添加序列化时间区域性设置

--- exceptionless.webhooks/Web/Startup.cs
+++ exceptionless.webhooks/Web/Startup.cs
@@ -2,8 +2,8 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using NLog.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
 using Rabbit.Extensions.DependencyInjection;
 using System;
 using System.Linq;
@@ -27,34 +27,39 @@ namespace Web
         {
             var appSettings = Configuration.Get<AppSettings>();
 
-            var enableModules = new[] { "Exceptionless.WebHook.Abstractions" }.Concat(appSettings.EnableModules)
+            var enableModules = new[] { "ExceptionLess.WebHook.Abstractions" }.Concat(appSettings.EnableModules)
                 .ToDictionary(name => name, name => Assembly.Load(new AssemblyName(name)));
 
             foreach (var item in enableModules.Where(i => i.Value == null))
                 Console.WriteLine($"无法加载模块：{item.Key}");
 
             var assemblies = enableModules.Values.ToArray();
+
             services
                 .AddLogging()
                 .AddMemoryCache()
+                .AddHttpClient()
                 .AddInterfaceDependency(assemblies)
                 .AddMvc();
+
+            JsonConvert.DefaultSettings = () => new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented,
+                ContractResolver = new CamelCasePropertyNamesContractResolver(),
+                DateTimeZoneHandling = DateTimeZoneHandling.Local
+            };
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            loggerFactory
-                .AddNLog();
-
             app.UseStatusCodePages();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app
-                .UseMiddleware<ExceptionlessWebhookMiddleware>();
+            app.UseMiddleware<ExceptionLessWebhookMiddleware>();
         }
     }
 }
